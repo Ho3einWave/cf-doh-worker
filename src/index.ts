cbde94a72236eca448323dabf1d24c501a786825
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2025-11-20 18:24:30 +0330

    feat: implemented a very minimalist  DNS-over-HTTPS proxy on Cloudflare Workers based on tina-hello/doh-cf-workers

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,57 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return handleRequest(request, env);
 	},
 } satisfies ExportedHandler<Env>;
+
+async function handleRequest(request: Request, env: Env): Promise<Response> {
+	const doh = env.doh;
+	const dohJson = env.dohjson;
+	const contype = env.contype;
+	const jsonType = env.jsontype;
+	const path = env.path;
+
+	const r404 = new Response(null, { status: 404 });
+
+	// when res is a Promise<Response>, it reduces billed wall-time
+	// blog.cloudflare.com/workers-optimization-reduces-your-bill
+	let res: Promise<Response> | Response = r404;
+	const { method, headers, url } = request;
+	const { searchParams, pathname } = new URL(url);
+
+	// Check path
+	// If path is set in env, restrict access to that path
+	if (path && !pathname.startsWith(path)) {
+		return r404;
+	}
+
+	if (method == 'GET' && searchParams.has('dns')) {
+		res = fetch(doh + '?dns=' + searchParams.get('dns'), {
+			method: 'GET',
+			headers: {
+				Accept: contype,
+			},
+		});
+	} else if (method === 'POST' && headers.get('content-type') === contype) {
+		// streaming out the request body is optimal than awaiting on it
+		const rostream = request.body;
+		res = fetch(doh, {
+			method: 'POST',
+			headers: {
+				Accept: contype,
+				'Content-Type': contype,
+			},
+			body: rostream,
+		});
+	} else if (method === 'GET' && headers.get('Accept') === jsonType) {
+		const search = new URL(url).search;
+		res = fetch(dohJson + search, {
+			method: 'GET',
+			headers: {
+				Accept: jsonType,
+			},
+		});
+	}
+	return res;
+}
